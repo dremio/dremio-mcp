a410236ee279c367b66a4028dedd7516d0f7d8b9
Aniket Kulkarni <aniket.kulkarni@dremio.com>
2025-08-27 22:07:57 -0400

    Support `mcp_tool_invocation` Counter and `mcp_tool_invocation_duration` Histogram

--- tests/e2e/test_e2e_pat.py
+++ tests/e2e/test_e2e_pat.py
@@ -15,9 +15,14 @@
 #
 import uuid
 
+import httpx
 import pytest
 from mcp.types import CallToolResult
 from conftest import http_streamable_client_server, http_streamable_mcp_server
+from urllib.parse import urlparse
+from dremioai.config import settings
+
+from dremioai.metrics.tool_metrics import invocation_counter
 
 
 @pytest.mark.asyncio
@@ -54,3 +59,23 @@ async def test_tool_pat(mock_config_dir, logging_server, logging_level, project_
                     assert (
                         str(project_id) in le.path
                     ), f"{le} does not have the right project id"
+
+        async with httpx.AsyncClient() as client:
+            u = urlparse(sf.mcp_server.url)._replace(path="/metrics/").geturl()
+            r = await client.get(u, headers={"Authorization": "Bearer my-token"})
+            assert (
+                r.status_code == 200
+            ), f"Error getting metrics: {r.text} status={r.status_code}"
+            if project_id is None:
+                project_id = settings.instance().dremio.project_id
+            for line in r.text.splitlines():
+                if (
+                    line.startswith(f"{invocation_counter._name}_total{{")
+                    and f'project_id="{project_id}"' in line
+                ):
+                    assert (
+                        float(line.split()[-1]) == 1.0
+                    ), f"Invocation count not 1: {line}"
+                    break
+            else:
+                assert False, f"Invocation count for {project_id} not found in {r.text}"
